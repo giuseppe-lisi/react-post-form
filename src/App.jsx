2257d8dfea45bf7d50a9e756a1f69e8256167f10
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-01-23 00:53:38 +0100

    creato two-way data binding tra il campo dell'autore e l'autore mostrato nel post

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,24 @@ function App() {
     const [body, setbody] = useState("Contenuto");
     const [isPublic, setIsPublic] = useState("publicOrNot")
 
+    function handleChange(e) {
+        const newValue = e.target.value;
+        setAuthor(newValue);
+    }
+
     return (
         <>
             <h1>Blog</h1>
-            <div>{author}</div>
+            <input type="text" placeholder="autore" onChange={ e => handleChange(e)}/>
             <div>{title}</div>
             <div>{body}</div>
             <div>{isPublic}</div>
+
+            <h1>Post</h1>
+
+            <div className="card">
+                <h2>{author}</h2>
+            </div>
         </>
     );
 }
