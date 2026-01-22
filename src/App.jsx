f033e2d4d2967f0a989be76bafca0d92515e5ef8
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-01-23 00:49:26 +0100

    gli stati vengono stampati in pagina

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,18 @@ import { useState } from "react";
 
 function App() {
 
+    const [author, setAuthor] = useState("Autore");
+    const [title, settitle] = useState("Titolo");
+    const [body, setbody] = useState("Contenuto");
+    const [isPublic, setIsPublic] = useState("publicOrNot")
+
     return (
         <>
-            <h1>hello react world</h1>
+            <h1>Blog</h1>
+            <div>{author}</div>
+            <div>{title}</div>
+            <div>{body}</div>
+            <div>{isPublic}</div>
         </>
     );
 }
