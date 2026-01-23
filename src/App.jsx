d15fb74bd947b1f82cbbaa6a8fd2ce2189531de6
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-01-23 01:01:03 +0100

    ogni input influenza ora soltanto la parte corrispondente della card

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,33 @@ import { useState } from "react";
 function App() {
 
     const [author, setAuthor] = useState("Autore");
-    const [title, settitle] = useState("Titolo");
-    const [body, setbody] = useState("Contenuto");
+    const [title, setTitle] = useState("Titolo");
+    const [body, setBody] = useState("Contenuto");
     const [isPublic, setIsPublic] = useState("publicOrNot")
 
-    function handleChange(e) {
-        const newValue = e.target.value;
-        setAuthor(newValue);
+    function changeAuthor(e) {
+        setAuthor(e.target.value);
+    }
+    function changeTitle(e) {
+        setTitle(e.target.value);
+    }
+    function changeBody(e) {
+        setBody(e.target.value);
     }
 
     return (
         <>
             <h1>Blog</h1>
-            <input type="text" placeholder="autore" onChange={ e => handleChange(e)}/>
-            <div>{title}</div>
-            <div>{body}</div>
-            <div>{isPublic}</div>
+            <input placeholder="autore" onChange={ e => changeAuthor(e)}/>
+            <input placeholder="titolo" onChange={ e => changeTitle(e)}/>
+            <input placeholder="contenuto" onChange={ e => changeBody(e)}/>
 
             <h1>Post</h1>
 
             <div className="card">
                 <h2>{author}</h2>
+                <h4>{title}</h4>
+                <p>{body}</p>
             </div>
         </>
     );
