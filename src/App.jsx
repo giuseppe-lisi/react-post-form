415f8ed9349ac353ab148aede844b387813233e5
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-01-23 01:09:17 +0100

    il contenuto placeholder nel post viene ora stampato dalla variabile di stato post che è un oggetto contenente tutto il contenuto del post

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,14 @@ import { useState } from "react";
 
 function App() {
 
-    const [author, setAuthor] = useState("Autore");
-    const [title, setTitle] = useState("Titolo");
-    const [body, setBody] = useState("Contenuto");
     const [isPublic, setIsPublic] = useState("publicOrNot")
 
-    function changeAuthor(e) {
-        setAuthor(e.target.value);
-    }
-    function changeTitle(e) {
-        setTitle(e.target.value);
-    }
-    function changeBody(e) {
-        setBody(e.target.value);
-    }
+    const [post, setPost] = useState({
+        author : "autore",
+        title : "title",
+        body : "body"
+    })
+
 
     return (
         <>
@@ -26,11 +20,11 @@ function App() {
 
             <h1>Post</h1>
 
-            <div className="card">
-                <h2>{author}</h2>
+            <div className="post">
+                <h2>{post.author}</h2>
                 <hr />
-                <h4>{title}</h4>
-                <p>{body}</p>
+                <h4>{post.title}</h4>
+                <p>{post.body}</p>
             </div>
         </>
     );
